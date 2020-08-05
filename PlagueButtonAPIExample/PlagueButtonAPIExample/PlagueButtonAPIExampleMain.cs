813055754475c96e9e4648aa1195bf40c2884ef8
Plague <36628963+OFWModz@users.noreply.github.com>
2020-08-05 14:00:30 +0100

    Example Visual Studio Project

--- PlagueButtonAPIExample/PlagueButtonAPIExample/PlagueButtonAPIExampleMain.cs
+++ PlagueButtonAPIExample/PlagueButtonAPIExample/PlagueButtonAPIExampleMain.cs
@@ -2,44 +2,37 @@ using MelonLoader;
 using System;
 using UnityEngine;
 using PlagueButtonAPI;
+using UnityEngine.UI;
 
 namespace PlagueButtonAPIExample
 {
     public class PlagueButtonAPIExampleMain : MelonMod
     {
-        public static GameObject SubMenu_1;
-
-        public static GameObject SubMenu_2;
-
         public override void VRChat_OnUiManagerInit()
         {
-            SubMenu_1 = ButtonAPI.MakeEmptyPage("SubMenu_1");
-
-            SubMenu_2 = ButtonAPI.MakeEmptyPage("SubMenu_2");
-
             ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Utils", "Some Random Utils.", ButtonAPI.HorizontalPosition.LeftOfMenu, ButtonAPI.VerticalPosition.TopButton, null, delegate (bool a)
             {
-                ButtonAPI.EnterSubMenu(SubMenu_1);
+                ButtonAPI.EnterSubMenu(ButtonAPI.MakeEmptyPage("SubMenu_1"));
             }, Color.magenta, null, false, true, true);
 
             //SubMenu_1 Contents
-            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Next Page", "Goes To The Next Page.", ButtonAPI.HorizontalPosition.LeftOfMenu, ButtonAPI.VerticalPosition.TopButton, SubMenu_1.transform, delegate (bool a)
+            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Next Page", "Goes To The Next Page.", ButtonAPI.HorizontalPosition.LeftOfMenu, ButtonAPI.VerticalPosition.TopButton, ButtonAPI.MakeEmptyPage("SubMenu_1").transform, delegate (bool a)
             {
-                ButtonAPI.EnterSubMenu(SubMenu_2);
+                ButtonAPI.EnterSubMenu(ButtonAPI.MakeEmptyPage("SubMenu_2"));
             }, Color.magenta, null, false, false, true);
 
-            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Toggle, "Toggle Pickups", "Toggles All Pickups In The Current Instance.", ButtonAPI.HorizontalPosition.FirstButtonPos, ButtonAPI.VerticalPosition.TopButton, SubMenu_1.transform, delegate (bool a)
+            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Toggle, "Toggle Pickups", "Toggles All Pickups In The Current Instance.", ButtonAPI.HorizontalPosition.FirstButtonPos, ButtonAPI.VerticalPosition.TopButton, ButtonAPI.MakeEmptyPage("SubMenu_1").transform, delegate (bool a)
             {
                 TogglePickups(a);
             }, Color.magenta, null, false, false, true);
 
             //SubMenu_2 Contents
-            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Prev Page", "Goes To The Previous Page.", ButtonAPI.HorizontalPosition.LeftOfMenu, ButtonAPI.VerticalPosition.TopButton, SubMenu_2.transform, delegate (bool a)
+            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Prev Page", "Goes To The Previous Page.", ButtonAPI.HorizontalPosition.LeftOfMenu, ButtonAPI.VerticalPosition.TopButton, ButtonAPI.MakeEmptyPage("SubMenu_2").transform, delegate (bool a)
             {
-                ButtonAPI.EnterSubMenu(SubMenu_1);
+                ButtonAPI.EnterSubMenu(ButtonAPI.MakeEmptyPage("SubMenu_1"));
             }, Color.magenta, null, false, false, true);
 
-            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Hello World", "Prints Hello World To Console.", ButtonAPI.HorizontalPosition.FirstButtonPos, ButtonAPI.VerticalPosition.TopButton, SubMenu_2.transform, delegate (bool a)
+            ButtonAPI.CreateButton(ButtonAPI.ButtonType.Default, "Hello World", "Prints Hello World To Console.", ButtonAPI.HorizontalPosition.FirstButtonPos, ButtonAPI.VerticalPosition.TopButton, ButtonAPI.MakeEmptyPage("SubMenu_2").transform, delegate (bool a)
             {
                 MelonModLogger.Log(ConsoleColor.Magenta, "Hello World!");
             }, Color.magenta, null, false, false, true);
