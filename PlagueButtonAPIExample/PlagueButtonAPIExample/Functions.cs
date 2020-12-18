46cbc7f64a9fdb79180236eed6ba353206def680
Plague <36628963+OFWModz@users.noreply.github.com>
2020-12-18 12:12:42 +0000

    Example Visual Studio Project

--- PlagueButtonAPIExample/PlagueButtonAPIExample/Functions.cs
+++ PlagueButtonAPIExample/PlagueButtonAPIExample/Functions.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VRC.SDKBase;
 
+namespace PlagueButtonAPIExample
+{
+    internal class Functions
+    {
+        internal static void TogglePortals(bool state)
+        {
+            var array = Resources.FindObjectsOfTypeAll<PortalInternal>();
+            foreach (var i in array)
+            {
+                if (i == null)
+                {
+                    continue;
+                }
+
+                i.enabled = state;
+                i.gameObject.SetActive(state);
+
+                if (!state)
+                {
+                    Networking.Destroy(i.gameObject);
+                }
+            }
+        }
+    }
+}
