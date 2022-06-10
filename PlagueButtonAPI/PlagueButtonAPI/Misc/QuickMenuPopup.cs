87f5cefb2071ddaf65a9f1b9008675b0ebdb51ec
MistressPlague <krewellamusicps3@gmail.com>
2022-06-10 02:23:54 +0100

    Primitive Popup Class

---
+++ PlagueButtonAPI/PlagueButtonAPI/Misc/QuickMenuPopup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlagueButtonAPI.Pages;
+using Object = UnityEngine.Object;
+
+namespace PlagueButtonAPI.Misc
+{
+    public class QuickMenuPopup
+    {
+        public MenuPage Page;
+
+        public static QuickMenuPopup CreatePopup(string Title)
+        {
+            var instance = new QuickMenuPopup
+            {
+                Page = MenuPage.CreatePage("TempPopupPage", Title, false, false, Gridify: true)
+            };
+
+            instance.Page.page.gameObject.AddComponent<ObjectHandler>().OnDisabled += _ =>
+            {
+                Object.Destroy(instance.Page.page.gameObject);
+            };
+        }
+
+        public void Show()
+        {
+            Page.OpenMenu();
+        }
+
+        public void Close()
+        {
+            Page.CloseMenu();
+        }
+    }
+}
